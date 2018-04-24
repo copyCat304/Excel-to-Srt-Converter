e0112d89c7f04becf918a9ea9d4a917124737029
Vishal Pardeshi <vishalpardeshi48@gmail.com>
2018-04-24 22:40:39 +0530

    RelianceInfra
    reliance changes added village name
    
    Co-Authored-By: Vishal Pardeshi <copycat304@users.noreply.github.com>

--- nwrSub.py
+++ nwrSub.py
@@ -3,7 +3,7 @@ import os
 from tkinter import *
 from tkinter import ttk
 
-#working directory
+#working directory C:\Users\sarveli\Desktop\VSRP_WorkingSheet Rev 03.xlsx    4 4 53 14692
 os.chdir('C:\\Users\\sarveli\\Desktop')
 def showBeginsHere():
     #get excel file path
@@ -11,7 +11,7 @@ def showBeginsHere():
     #opening xl workbook
     nwr = openpyxl.load_workbook(inputFilePath)
     #assigning and opening xl worksheet
-    sheet1 = nwr.get_sheet_by_name('x')
+    sheet1 = nwr.get_sheet_by_name('Final')
     #creating and opening txt file
     text = "sampleText.txt"
     saveFile = open(text, "w")
@@ -20,13 +20,13 @@ def showBeginsHere():
     mm = int(minutes.get())
     ss = int(seconds.get())
     #taking gps cordinate from text block of GUI
-    gpsCounts = int(totalFrame.get())
+    #gpsCounts = int(totalFrame.get())
     #converting time into sec
     hhs = int(hh) * 3600
     mms = int(mm) * 60
     videoLength = hhs + mms + ss
     #divide content of subtitles equally
-    frame = round(videoLength / gpsCounts, 2)
+    frame = videoLength
     #flag denotes iteration of variable videoLength in while loop
     flag = 0.0
     #f1, f2 and f3 are use to hold previous time value
@@ -39,19 +39,47 @@ def showBeginsHere():
     i = 1
     #shows status of program
     status = " Program Started "
-    
+    #print(status)
     #writing txt file copying value of xl
     while ( flag < videoLength):
+        #print(i)
+        #chainage
         x = sheet1.cell(row=i, column=1).value
+        #Easting
         y = sheet1.cell(row=i, column=2).value
+        #Northing
         z = sheet1.cell(row=i, column=3).value
-        x = round(x , 5)
-        y = round(y , 5)
+        #village 1
+        c = sheet1.cell(row=i, column=4).value
+        #village 2
+        d = sheet1.cell(row=i, column=5).value
+        #Timing
+        e = sheet1.cell(row=i, column=7).value
         #if cell is empty break the loop
-        if x == None and y == None:
+        if e == None:
             print("breaking becaue of if")
             status = "breaking becaue of if"
             break
+        '''
+        if x == None:
+            x = " "
+
+        if y == None:
+            y = " "
+
+        if z == None:
+            z = " "
+
+        if c == None:
+            c = " "
+
+        if d == None:
+            d = " "
+        ''' 
+        
+       
+        
+        
         #time division accordinf to flag
         t1 = int(flag/3600)
         b = int(flag%3600)
@@ -59,8 +87,36 @@ def showBeginsHere():
         t3 = int(b%60)
         #writing in txt file .zfill(2) is to make 1--->01 i.e it adds zero automatically
         saveFile.write(str(ft) + "\n" + str(f1).zfill(2) +":"+ str(f2).zfill(2)+":"+
-                       str(f3).zfill(2)+" --> "+str(t1).zfill(2)+":"+str(t2).zfill(2)+":"+str(t3).zfill(2)
-                       +"\n" + "Lon " + str(x) + "; Lat " + str(y) + "; FH : "+ str(z) + "\n")
+                       str(f3).zfill(2)+" --> "+str(t1).zfill(2)+":"+str(t2).zfill(2)+":"+str(t3).zfill(2))
+
+        if x == None:
+            saveFile.write("\n" )
+
+        if x != None:
+            x = round(x)
+            saveFile.write("\nCH : "+str(x))
+            #print("d=="+str(d))
+        
+        if y != None and z != None:
+            y = round(y , 2)
+            z = round(z , 2)
+            saveFile.write("; E : " + str(y) + "; N : "+ str(z))
+            #print("d=="+str(d))
+            
+
+        if c != None or d != None:
+            if c != None:
+                saveFile.write("\nVillage :  "+str(c))
+            if d != None:
+                if c == None:
+                    saveFile.write("\nVillage :  "+str(d)+"\n")
+                else:
+                    saveFile.write("; "+str(d)+"\n")
+
+                
+            saveFile.write("\n")
+
+        
         saveFile.write("\n")
         #assigning previous values
         f1 = t1
@@ -69,10 +125,11 @@ def showBeginsHere():
         #incrementing frame count
         ft = ft +1
         #incrementing time
-        flag = flag + frame 
+        flag = flag + 1 
         #pointing towards next cell in xl
         i = i + 1 
     status = " Program End "
+    #print(status)
     saveFile.close()
 
 #call TK() function for creating GUI
@@ -86,7 +143,7 @@ totalFrame = StringVar()
 inputDataFilePath = StringVar()
 
 #giving title and dimension to GUI
-root.wm_title("When__PEGION__Fly")
+root.wm_title("Now_at_Pigeon_Innovative")
 root.wm_geometry("500x500")
 
 #creating a frame
@@ -131,12 +188,12 @@ labelVidDuration = Label(frame, text = "Duration of Video (s)")
 labelVidDuration.grid(row = 4, column = 0, sticky = E)
 entryVidDuration = Entry(frame, textvariable = videoLength, width = 50)
 entryVidDuration.grid(row = 4, column = 1, sticky = E)
-"""
+
 labelTotalFrame = Label(frame, text = "Enter Total nos. of Frame", width =30)
 labelTotalFrame.grid(row = 5, column = 0, sticky = E)
 entryTotalFrame = Entry(frame, textvariable = totalFrame, width = 50)
 entryTotalFrame.grid(row = 5, column = 1, sticky = E)
-"""
+
 labelStatus = Label(frame, text = "Status")
 labelStatus.grid(row = 6, column = 0, sticky = E)
 entryStatus = Entry(frame, textvariable = status, width = 50)
